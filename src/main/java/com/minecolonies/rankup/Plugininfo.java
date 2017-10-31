8574d5094b677483b6f1433b96e42804280a8128
Asherslab <Asher.parry@icloud.com>
2017-10-31 16:46:50 +1000

    revert tests

--- src/main/java/com/minecolonies/rankup/Plugininfo.java
+++ src/main/java/com/minecolonies/rankup/Plugininfo.java
@@ -8,7 +8,7 @@ public class Plugininfo
     private Plugininfo() {}
 
     public static final String ID          = "rankup";
-    public static final String NAME        = "@name@";
+    public static final String NAME        = "Rankup";
     public static final String VERSION     = "@version@";
     public static final String DESCRIPTION = "@description@";
     public static final String URL         = "http://github.com/Minecolonies/Rankup";
